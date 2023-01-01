d6d27cca51c06ef825a4ce39780ba3a71219a887
tran van duc <22010159@st.phenikaa-uni.edu.vn>
2023-01-01 16:37:47 +0700

    1/1/2023

---
+++ video1.html/3/3.js
@@ -0,0 +1,15 @@
+let a = 'dan'
+console.log(a)
+console.log('a'+ 4 +5 )
+console.log(4 + 5 + 'a')
+let text = "Microsoft vo dich";
+console.log(text.replace("Microsoft","ngu vcl"));
+let b = 5;
+let c = 8;
+let d = '5';
+let e = +d;
+console.log(b === e, 'b: ', typeof b, 'd: ', typeof e)
+
+console.log('e value: ', e, 'typeof: ', typeof e)
+
+
