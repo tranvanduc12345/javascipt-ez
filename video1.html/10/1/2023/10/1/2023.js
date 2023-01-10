62c37c89650999dd91f808c45f15e20de371f43e
tran van duc <22010159@st.phenikaa-uni.edu.vn>
2023-01-10 19:59:04 +0700

    bai1 chuowng 3

---
+++ video1.html/10/1/2023/10/1/2023.js
@@ -0,0 +1,21 @@
+//forloop
+let people = ['btc', 'sol', 'near', 'eth', 'rlc']
+var i = 0;
+for (var i =0; i <= people.length; i++) {
+    console.log('so tt = ', people[i])
+}
+console.log('do dai bien: ', i)
+
+//tinh toan
+let b = 2;
+let h = 4;
+let z = b + h;
+let f = z - 2;
+console.log('people: ', 'h :', h, 'f: ', f, f ===h)
+
+//array
+let r = [
+    {Name:'duc', age:19},
+    {address:'ha nam', xom:1}
+]
+console.log('khai bao bien r:', r)
